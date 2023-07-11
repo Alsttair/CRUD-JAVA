a6fde403ad68656e986787f2fcdbd11d35edf56e
Alsttair <107138004+Alsttair@users.noreply.github.com>
2023-07-11 20:54:03 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,85 +1,74 @@
-const apiBaseUrl = 'https://649a1d4a79fbe9bcf8404b5a.mockapi.io/users/20201214010018/products';
+const baseUrl = 'https://649a1d4a79fbe9bcf8404b5a.mockapi.io/users/20201214010018/products';
 
-async function createSale(albumId, quantity) {
-  try {
-    const response = await fetch(`${apiBaseUrl}/sales`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        albumId,
-        quantity,
-      }),
-    });
+const albumForm = document.getElementById('albumForm');
+const albumList = document.getElementById('albumList');
 
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error('Erro ao criar venda:', error);
-    throw error;
-  }
+// Função para renderizar os dados dos álbuns
+function renderAlbum(album) {
+  const row = document.createElement('tr');
+  row.innerHTML = `
+    <td>${album.name}</td>
+    <td>${album.price}</td>
+    <td>
+      <button onclick="editAlbum(${album.id})">Editar</button>
+      <button onclick="deleteAlbum(${album.id})">Excluir</button>
+    </td>
+  `;
+  albumList.appendChild(row);
 }
 
-async function getSales() {
-  try {
-    const response = await fetch(`${apiBaseUrl}/sales`);
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error('Erro ao obter vendas:', error);
-    throw error;
-  }
+// Função para exibir todos os álbuns existentes
+function displayAlbums() {
+  albumList.innerHTML = '';
+
+  // Simulando uma chamada à API para obter os dados dos álbuns
+  const albums = [
+    { id: 1, name: 'Fearless', price: 29.99 },
+    { id: 2, name: '1989', price: 24.99 },
+    { id: 3, name: 'Red', price: 19.99 }
+  ];
+
+  albums.forEach((album) => {
+    renderAlbum(album);
+  });
 }
 
-async function updateSale(saleId, quantity) {
-  try {
-    const response = await fetch(`${apiBaseUrl}/sales/${saleId}`, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        quantity,
-      }),
-    });
+// Função para adicionar um novo álbum
+function addAlbum(name, price) {
+  // Simulando uma chamada à API para adicionar o álbum
+  const newAlbum = {
+    id: Date.now(),
+    name: name,
+    price: price
+  };
 
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error('Erro ao atualizar venda:', error);
-    throw error;
-  }
+  // Adicionar o álbum à lista
+  renderAlbum(newAlbum);
 }
 
-async function deleteSale(saleId) {
-  try {
-    const response = await fetch(`${apiBaseUrl}/sales/${saleId}`, {
-      method: 'DELETE',
-    });
+// Função para lidar com o envio do formulário
+albumForm.addEventListener('submit', (e) => {
+  e.preventDefault();
 
-    if (response.status === 204) {
-      console.log('Venda excluída com sucesso!');
-    } else {
-      throw new Error('Falha ao excluir venda');
-    }
-  } catch (error) {
-    console.error('Erro ao excluir venda:', error);
-    throw error;
-  }
+  const albumName = document.getElementById('albumName').value;
+  const albumPrice = document.getElementById('albumPrice').value;
+
+  addAlbum(albumName, albumPrice);
+
+  albumForm.reset();
+});
+
+// Função para editar um álbum
+function editAlbum(albumId) {
+  // Simulando uma chamada à API para editar o álbum
+  console.log(`Editar álbum com o ID: ${albumId}`);
 }
 
-createSale(1, 3)
-  .then((sale) => console.log('Venda criada:', sale))
-  .catch((error) => console.error('Erro ao criar venda:', error));
+// Função para excluir um álbum
+function deleteAlbum(albumId) {
+  // Simulando uma chamada à API para excluir o álbum
+  console.log(`Excluir álbum com o ID: ${albumId}`);
+}
 
-getSales()
-  .then((sales) => console.log('Vendas:', sales))
-  .catch((error) => console.error('Erro ao obter vendas:', error));
-
-updateSale(1, 5)
-  .then((sale) => console.log('Venda atualizada:', sale))
-  .catch((error) => console.error('Erro ao atualizar venda:', error));
-
-deleteSale(1)
-  .catch((error) => console.error('Erro ao excluir venda:', error));
+// Exibir os álbuns ao carregar a página
+displayAlbums();
